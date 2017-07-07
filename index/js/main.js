b1ccd156d475b71c2f547c7d516f890f58988b02
suary <604791489@qq.com>
2017-07-07 20:32:57 +0800

    up

--- index/js/main.js
+++ index/js/main.js
@@ -1,114 +1,48 @@
-
-
-function quitlogin(){
-    $.cookie('username',null,{path:'/'});
-    location.href='/login'
-}
-var skipcount = 0;
-function gotocenter(){
-    $("#userinfo").show();
-    $(".miantext").hide();
-    $.ajax({
-        url: "apis/selectuserinfo",
-        async: false,
-        data: {
-            partyid: $.cookie('partyid')
-        }
-    }).then(function (data) {
-        data = JSON.parse(data)
-        data=data.data;
-        console.log(data)
-        var templeteuserinfo=''
-        templeteuserinfo+='<h1>'+data.username+'的个人中心</h1>'
-        templeteuserinfo+='<h4>性别：'+(data.sex=='man'?'男':'女')+'</h4>'
-        templeteuserinfo+='<h4>个人宣言：'+data.slogun+'</h4>'
-        templeteuserinfo+='<h4>所在地：'+data.province+'省'+data.city+'市'+'</h4>'
-        templeteuserinfo+='<h4 onclick="return returntomian()">返回主页</h4>'
-
-        $("#userinfo").html(templeteuserinfo)
+var usermode = ''
+    
+    $("#sure").on('click', function () {
+        $("#phase1").css('display', 'none');
+        $("#phase2").css('display', '');
+        socket = io(location.hostname + ':880/' + $('input:radio:checked').val());
+        socket.on('userinfo', function (data) {
+                console.log(data)
+                beginway(data.usertype)
+            });
         
-        
+
     })
-}
-function returntomian(){
-    $("#userinfo").hide();
-    $(".miantext").show();
-}
-function newdata() {
-    $.ajax({
-        url: "apis/fetchlist",
-        async: false,
-        data: {
-            id: $("#keyword").val(),
-            skipcount: skipcount,
-            pagesize: 10
+    $("#sure2").on('click', function () {
+        if ($('input:radio:checked').val()) {
+            $("#phase2").css('display', 'none');
+            $("#phase3").css('display', '')
+            socket.emit('new user', { username: $("#yourname").val()});
+            
+            socket.on('new message', function (data) {
+                console.log(data)
+                $("#textarea").html($("#textarea").html() + data.name + ':' + data.text + '<br>')
+            });
+            
         }
-    }).then(function (data) {
-        data = JSON.parse(data)
-        var tatalCount=data.count
-        data=JSON.parse(data.data);
-        $("#router").html('')
-        var th = '<table class="table table-hover"><thead><tr><th class="w200">序号</th>'
-        for (x in data[0]) {
-            th += '<th class="w200">' + x + '</th>'
-        }
-        th += '</tr></thead><tbody>'
-        console.log(th)
-        for (x in data) {
-            th += '<tr><th>' + (parseInt(x)+parseInt(skipcount)+1) + '</th>'
-            for (y in data[x]) {
-                th += '<th>' + (data[x][y] === null ? '' : data[x][y]) + '</th>'
+    })
+    $("#submit").on('click', function () {
+        debugger
+        console.log(usermode)
+        if (usermode == 'fst' || usermode == 'sec') {
+            if ($("#input").val()) {
+                socket.emit('input event', { name: $("#yourname").val(), text: $("#input").val() });
             }
-            th += '</tr>'
+        } else {
+            alert('当前聊天室人数已经超过两人，您已经被限制发言')
         }
-        $("#router").append(th + '</tbody></table>')
-        setpage(tatalCount,skipcount)
     })
-};
-function setpage(count,skip){
-    $(".pagination").html('')
-    page=skip/10+1;
-    var pagenation ='<li><a aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>'
-    for(x = 1;((x-1)*10)<count;++x){
-        if (x==page) {
-            pagenation+='<li class="active page"><a>'+x+'</a></li>'
-        }else{
-            pagenation+='<li><a class="page">'+x+'</a></li>'
+    function beginway(type) {
+        if (type == 'whiteuser') {
+            usermode = 'fst'
+            waitmove = true
+        } else if (type == 'blackuser') {
+            usermode = 'sec'
+            waitmove = false
+        } else {
+            usertype = 'none'
         }
     }
-     pagenation+='<li><a aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>'
-    
-    $(".pagination").append(pagenation)
-    $(".page").on('click', function (event) {
-        skipcount = (event.target.innerHTML-1)*10;
-        newdata()
-    });
-}
-
-
-$("#fetch").on('click', function () {
-    skipcount = 0
-    newdata()
-});
-function submit22() {
-    $.ajax({
-        url: "apis/insertnode",
-        async: false,
-        data: {
-            id: $("#id").val(),
-            text: $("#text").val()
-        }
-    }).then(function (data) {
-        newdata()
-    })
-}
-$("#submit2").on('click', function () {
-    submit22()
-});
-if((!$.cookie('username'))||$.cookie('username')=='null'){
-			location.href='/login'
-}else{
-    $("#loginman").html($.cookie('username')+'<span class="caret"></span>')
-    $("#self").attr('href','/partycenter/?id='+$.cookie('partyid'))
-    newdata()
-}
